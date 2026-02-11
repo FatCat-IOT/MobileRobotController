d43e8cf441ca08db79dd88f02bca5570a545e80a
Danny Sneham <dannyabner1999@gmail.com>
2026-02-11 16:13:49 +0100

    Update preload.js

--- electron-flask-clone-win32-x64(UDP)/resources/app/src/preload.js
+++ electron-flask-clone-win32-x64(UDP)/resources/app/src/preload.js
@@ -2,7 +2,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 const fs = require('fs');
 const path = require('path');
 
-// ✅ Same folder as preload.js
+// Same folder as preload.js
 const configPath = path.join(__dirname, 'config.json');
 const config = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
 
